e9d4937d4d4aa0715cab7e3521f34c21948dbfcf
theathleticnerd <jobandjeffy@gmail.com>
2021-02-11 10:13:35 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { animateScroll as scroll } from "react-scroll";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIconLink,
+  SocialIcons,
+} from "./FooterElements";
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Carrers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/signin">Submit Video</FooterLink>
+              <FooterLink to="/signin">More MindHack</FooterLink>
+              <FooterLink to="/signin">MindHack Summit 2020</FooterLink>
+              <FooterLink to="/signin">Team</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/signin">Conatct</FooterLink>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">Ambassadors</FooterLink>
+              <FooterLink to="/signin">Agency</FooterLink>
+              <FooterLink to="/signin">Influencer</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">YouTube</FooterLink>
+              <FooterLink to="/signin">Twitter</FooterLink>
+              <FooterLink to="/signin">LinkdIn</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/" onClick={toggleHome}>
+              WebHunt
+            </SocialLogo>
+            <WebsiteRights>
+              WebHunt © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href="//www.twitter.com"
+                target="_blank"
+                aria-label="Twitter"
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkdin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import { Button } from "../ButtonElements";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Lighting Talks</HeroH1>
+        <HeroP>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
